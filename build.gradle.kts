213bf4fcc1371f2123eb3798974e2ffb6bdc4137
Jocude <jorge.cuevasd@gmail.com>
2026-02-07 01:19:38 +0100

    ADD: Camara sin interfaz

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,4 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
+
 buildscript {
     repositories {
         google()
@@ -10,14 +10,7 @@ buildscript {
     }
 }
 
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-        maven { url = uri("https://jitpack.io") }
-    }
+tasks.register("clean", Delete::class) {
+    delete(rootProject.layout.buildDirectory)
 }
 
-tasks.register("clean", Delete::class) {
-    delete(rootProject.buildDir)
-}
